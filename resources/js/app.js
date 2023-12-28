316a1f29478d2bee13fa1184331dabf94393d5ef
fadillah wahyu <fadillahzx404@gmail.com>
2023-12-28 21:03:25 +0700

    Com

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,5 @@
 import "./bootstrap";
 
-import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
-
 import Datepicker from "flowbite-datepicker/Datepicker";
 
 const datepickerEl = document.getElementById("datepickerId");
